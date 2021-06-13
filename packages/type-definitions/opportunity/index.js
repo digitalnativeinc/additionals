85f2cae982eb34e557df20c7942d7c0d2ec0f08d
junghoon lee <billjhlee@gmail.com>
2021-06-13 23:05:35 +0900

    update: types

--- packages/type-definitions/opportunity/index.js
+++ packages/type-definitions/opportunity/index.js
@@ -1,22 +1,38 @@
 const opportunityType = {
   types: [
     {
-      AccountInfo: 'AccountInfoWithTripleRefCount',
-      Address: 'MultiAddress',
-      LookupSource: 'MultiAddress',
-      XCurrencyId: {
-        chain_id: 'ChainId',
-        currency_id: 'Bytes',
+      // on all versions
+      minmax: [0, undefined],
+      types: {
+        AccountInfo: 'AccountInfoWithTripleRefCount',
+        Address: 'MultiAddress',
+        LookupSource: 'MultiAddress',
+        XCurrencyId: {
+          chain_id: 'ChainId',
+          currency_id: 'Bytes',
+        },
+        CurrencyIdOf: 'CurrencyId',
+        CurrencyId: {
+          _enum: {
+            Token: 'TokenSymbol',
+          },
+        },
+        TokenSymbol: {
+          _enum: ['ACA', 'AUSD', 'DOT', 'XBTC', 'LDOT', 'RENBTC', 'SDN', 'PLM'],
+        },
+        AmountOf: 'Amount',
+        Amount: 'i128',
+        DataVersion: 'u64',
+        RequestIdentifier: 'u64',
+        SpecIndex: 'Vec<u8>',
+        CDP: {
+          liquidation_fee: 'Balance',
+          max_collateraization_rate: 'U256',
+          stability_fee: 'Balance',
+        },
       },
-      CurrencyIdOf: 'U32',
-      CurrencyId: 'U32',
-      TokenSymbol: {
-        _enum: ['ACA', 'AUSD', 'DOT', 'XBTC', 'LDOT', 'RENBTC', 'SDN', 'PLM'],
-      },
-      AmountOf: 'Amount',
-      Amount: 'i128',
     },
   ],
 };
 
-export default opportunityType
+export default opportunityType;
